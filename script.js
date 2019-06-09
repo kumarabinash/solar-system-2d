86a96e2afb03ef4c25b66200cc8a01969bb42951
Kumar Abinash <abinash.b2@gmail.com>
2019-06-09 15:36:45 +0530

    Added function to toggle animation & labels

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get window Height & Width
-const windowHeight = window.innerHeight;
+const windowHeight = window.innerHeight - 70;
 const windowWidth = window.innerWidth;
 
 // Get min value to make a square
@@ -21,39 +21,61 @@ const earth = $("#earth");
 const center_x = parseFloat($(sun).css('left').split('px')[0]);
 const center_y = parseFloat($(sun).css('top').split('px')[0]);
 
-
-// Initial radian of earth
-const theta = 0; 
-
 // Initial Position of earth
 var earth_position_x = center_x + innerRadius;
 var earth_position_y = center_y;
 
+var earth_timer;
+
 // Set earth at initial position
 $(earth).css('left',earth_position_x).css('top', earth_position_y);
 
-// Formula get x & y coordinates
-//(𝑥,𝑦) = (𝑎 + 𝑟cos𝜃, 𝑏 + 𝑟sin𝜃)
-// Where a & b are initial coordinates
-// r is radius
-// 𝜃 is the new angle in radian
 
-// To get coordinate in our code it will be
-// (center_x, center_y) = (center_x + innerRadius * Math.cos(theta + radian_increment), center_y + innerRadius * Math.sin(theta + radian_increment))
 
-var degree = 0;
 
-setInterval(function(){
-  ++degree;
+$("#btn-toggle-animation").on('click', function(e){
+  e.preventDefault();
+  toggleRevolutionEarth();
+});
 
-  // If degree reaches 361, bring it back to zero
-  if(degree === 361)
-    degree = 0;
+$("#btn-toggle-labels").on('click', function(e){
+  e.preventDefault();
+  $(".celestial-bodies .label").toggleClass('hidden');
+});
 
-  // Conver degree into radian
-  radian = degree *  Math.PI / 180;
+function toggleRevolutionEarth(){
+  if(earth_timer){
+    clearInterval(earth_timer);
+    earth_timer = false;
+    return
+  }
 
-  earth_position_x = center_x + innerRadius * Math.cos(theta + radian);
-  earth_position_y = center_y + innerRadius * Math.sin(theta + radian);
-  $(earth).css('left',earth_position_x).css('top', earth_position_y);
-}, 50)
+  // Initial radian of earth
+  const theta = 0; 
+
+  // Formula get x & y coordinates
+  //(𝑥,𝑦) = (𝑎 + 𝑟cos𝜃, 𝑏 + 𝑟sin𝜃)
+  // Where a & b are initial coordinates
+  // r is radius
+  // 𝜃 is the new angle in radian
+
+  // To get coordinate in our code it will be
+  // (center_x, center_y) = (center_x + innerRadius * Math.cos(theta + radian_increment), center_y + innerRadius * Math.sin(theta + radian_increment))
+
+  var degree = 0;
+
+  earth_timer = setInterval(function(){
+    ++degree;
+
+    // If degree reaches 361, bring it back to zero
+    if(degree === 361)
+      degree = 0;
+
+    // Conver degree into radian
+    radian = degree *  Math.PI / 180;
+
+    earth_position_x = center_x + innerRadius * Math.cos(theta + radian);
+    earth_position_y = center_y + innerRadius * Math.sin(theta + radian);
+    $(earth).css('left',earth_position_x).css('top', earth_position_y);
+  }, 50)
+}
